001c79cb7d50a9e944c8336ba2e0dffd766901f2
anssshu <anssshu@gmail.com>
2019-11-16 13:11:33 +0530

    lappy

--- game/notes.gd
+++ game/notes.gd
@@ -2,5 +2,17 @@
 ##
 # 
 # all the common assets will be put inside the asset folder
-# object specific assets will be put inside the object folder itself
-# 
+# object specific assets will be put inside the object folder 
+
+# next job
+
+# complete stone throw 
+# create  the running throw for the player
+# create a puff of dust for the player
+# create the flying cloth on the player
+
+#chracters
+#bee
+#ant
+#insect for stage one
+#environment with tile sets
